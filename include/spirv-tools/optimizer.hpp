236d6ed61a5eaac80fa348db1cee3bbc00407470
Lukas Hermanns <lukas.hermanns@epicgames.com>
2020-11-12 13:23:12 -0500

    Add 'fused-multiply-add' pass to emulate invariant qualifier for older versions of Metal.

--- include/spirv-tools/optimizer.hpp
+++ include/spirv-tools/optimizer.hpp
@@ -933,6 +933,7 @@ Optimizer::PassToken CreateInterfaceVariableScalarReplacementPass();
 // from every function in the module.  This is useful if you want the inliner to
 // inline these functions some reason.
 Optimizer::PassToken CreateRemoveDontInlinePass();
+
 // Create a fix-func-call-param pass to fix non memory argument for the function
 // call, as spirv-validation requires function parameters to be an memory
 // object, currently the pass would remove accesschain pointer argument passed
@@ -978,6 +979,13 @@ Optimizer::PassToken CreateModifyMaximalReconvergencePass(bool add);
 // parameters into separate image and sampler parts. Binding numbers and
 // other decorations are copied.
 Optimizer::PassToken CreateSplitCombinedImageSamplerPass();
+
+// UE Change Begin: Implement a fused-multiply-add pass to reduce the
+// possibility of re-association. Create a pass that will fuse floating point
+// multiply+add pairs to avoid re-association of instructions.
+Optimizer::PassToken CreateFusedMultiplyAddPass();
+// UE Change End: Implement a fused-multiply-add pass to reduce the possibility
+// of re-association.
 
 // Creates a pass to remap bindings to avoid conflicts, assuming the module
 // is valid for Vulkan.  A conflict exits when an entry point uses two distinct
