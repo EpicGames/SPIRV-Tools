14c68e28f70b4f31fdc1e95675e32440c5fda7a3
Laura Hermanns <laura.hermanns@epicgames.com>
2022-04-19 09:22:56 -0400

    Added android_driver_patch_pass to circumvent Android driver validation errors.

--- source/opt/optimizer.cpp
+++ source/opt/optimizer.cpp
@@ -547,6 +547,10 @@ bool Optimizer::RegisterPassFromFlag(const std::string& flag,
   } else if (pass_name == "fused-multiply-add") {
     RegisterPass(CreateFusedMultiplyAddPass());
     // UE Change End: Implement a fused-multiply-add pass to reduce the
+    // UE Change Begin: Added support for Android driver patch pass to fix platform specific issues
+  } else if (pass_name == "android-driver-patch") {
+    RegisterPass(CreateAndroidDriverPatchPass());
+    // UE Change End: Added support for Android driver patch pass to fix platform specific issues
     // possibility of re-association.
   } else if (pass_name == "graphics-robust-access") {
     RegisterPass(CreateGraphicsRobustAccessPass());
@@ -1220,6 +1224,13 @@ Optimizer::PassToken CreateFusedMultiplyAddPass() {
 }
 // UE Change End: Implement a fused-multiply-add pass to reduce the possibility
 // of re-association.
+
+// UE Change Begin: Added support for Android driver patch pass to fix platform specific issues
+Optimizer::PassToken CreateAndroidDriverPatchPass() {
+  return MakeUnique<Optimizer::PassToken::Impl>(
+      MakeUnique<opt::AndroidDriverPatchPass>());
+}
+// UE Change End: Added support for Android driver patch pass to fix platform specific issues
 
 }  // namespace spvtools
 
